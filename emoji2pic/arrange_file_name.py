d5009a38357d2c97eba8fa55c90bf10cf435c06b
sniper-py <dzmbbs@qq.com>
2019-01-10 17:30:02 +0800

    arrange file name

---
+++ emoji2pic/arrange_file_name.py
@@ -0,0 +1,45 @@
+import os
+
+
+def arrange_file_name(file_dir='AppleEmoji'):
+    for root, dirs, files in os.walk(file_dir):
+        pass
+
+    unicode_to_path = dict()
+    initial_unicode = dict()
+
+    for file in files:
+        chip_list = file.split('_')
+        eng = len(chip_list[0]) + 1 if chip_list[1][:5] != 'emoji' else len(chip_list[0] + chip_list[1]) + 2
+        base_name = file[eng:-4].replace('_', '-')
+        base_name_chip_list = base_name.split('-')
+
+        unicode_chip_list = list()
+        for chip in base_name_chip_list:
+            if len(chip) == 2:
+                unicode_chip = 'x' + str(chip)
+            elif len(chip) == 4:
+                unicode_chip = 'u' + str(chip)
+            elif len(chip) == 5:
+                unicode_chip = 'U000' + str(chip)
+            else:
+                raise ValueError(chip)
+            unicode_chip_list.append(unicode_chip)
+
+        if initial_unicode.get(unicode_chip_list[0]) is None:
+            initial_unicode[unicode_chip_list[0]] = [len(unicode_chip_list)]
+        else:
+            if len(unicode_chip_list) not in initial_unicode[unicode_chip_list[0]]:
+                initial_unicode[unicode_chip_list[0]].append(len(unicode_chip_list))
+                initial_unicode[unicode_chip_list[0]] = sorted(initial_unicode[unicode_chip_list[0]], reverse=True)
+
+        unicode_name = '-'.join(unicode_chip_list)
+        unicode_to_path[unicode_name] = file
+
+    print(initial_unicode)
+    print(unicode_to_path)
+    return
+
+
+if __name__ == '__main__':
+    arrange_file_name()
